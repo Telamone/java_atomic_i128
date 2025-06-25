e6c11f97b0039feb1aecfc9d43da275daa5c4697
BuildTools <telami.giuseppe@gmail.com>
2025-06-25 12:22:39 +0200

    Emptied the Benchmark (cause: too unstable). Updated the documentation.

--- test/it/telami/commons/concurrency/atomic/Benchmark.java
+++ test/it/telami/commons/concurrency/atomic/Benchmark.java
@@ -4,18 +4,13 @@ import it.telami.commons.util.OperatingSystem;
 
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.VarHandle;
-import java.security.SecureRandom;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
 
-@SuppressWarnings({"resource", "ExtractMethodRecommender"})
+@SuppressWarnings({"resource"})
 final class Benchmark {
-    private static final int WARMUP_CYCLES = 100_000;
-    //128 -> Medium contention
-    private static final int TOTAL_CYCLES = 128 + WARMUP_CYCLES;
-    private static final long ADDEND = new SecureRandom().nextLong();
-    //Optimization for constant additions!
-    private static final long OPT = 0xffffffffffffffffL - ADDEND;
+    private static final int WARMUP_CYCLES = 40_000;
+    private static final int TOTAL_CYCLES = 1_000_000 + WARMUP_CYCLES;
 
     public static void main (final String[] args) {
         libraryImpl();
@@ -27,28 +22,13 @@ final class Benchmark {
         final i128 container = new i128();
         final Runnable r = OperatingSystem.currentOS == OperatingSystem.WINDOWS
                 ? () -> {
-            final i128 a = new i128(), b = new i128();
-            for (;;) {
-                a.copyFrom(container);
-                if (a.getLow() > OPT)
-                    b.set(a.getHigh() + 1, a.getLow() + ADDEND);
-                else b.setLow(a.getLow() + ADDEND);
-                VarHandle.fullFence();
-                if (container.compareAndSet(a, b))
-                    break;
-                else Thread.yield();
-            }
+
+
+
         } : () -> {
-            final i128 a = new i128(), b = new i128();
-            for (;;) {
-                container.getAcquire(a);
-                if (a.getLow() > OPT)
-                    b.set(a.getHigh() + 1, a.getLow() + ADDEND);
-                else b.setLow(a.getLow() + ADDEND);
-                if (container.weakCompareAndSetRelease(a, b))
-                    break;
-                else Thread.yield();
-            }
+
+
+
         };
         int i = -1;
         while (++i != WARMUP_CYCLES)
@@ -94,18 +74,9 @@ final class Benchmark {
         }
         final ref_container container = new ref_container();
         final Runnable r = () -> {
-            i128_ref a;
-            final i128_ref b = new i128_ref();
-            for (;;) {
-                a = (i128_ref) refVar.getAcquire(container);
-                if (a.low > OPT) {
-                    b.low = a.low + ADDEND;
-                    b.high = a.high + 1;
-                } else b.low = a.low + ADDEND;
-                if (refVar.weakCompareAndSetRelease(container, a, b))
-                    break;
-                else Thread.yield();
-            }
+
+
+
         };
         int i = -1;
         while (++i != WARMUP_CYCLES)
@@ -134,10 +105,9 @@ final class Benchmark {
         final i128 ref = new i128();
         final Runnable r = () -> {
             synchronized (ref) {
-                if (ref.low > OPT) {
-                    ref.low += ADDEND;
-                    ++ref.high;
-                } else ref.low += ADDEND;
+
+
+
             }
         };
         int i = -1;
